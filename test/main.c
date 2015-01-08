8ceb239bac8bc7c56d527b6093865ace1a4b0832
mclifton94 <mclifton@capital.edu>
2015-01-08 08:37:50 -0500

    Test
    Trying to use git

---
+++ test/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  test
+//
+//  Created by Mark Clifton on 1/8/15.
+//  Copyright (c) 2015 Mark Clifton. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    //This is a test.
+    return 0;
+}
